792d82cc5423af3adc0dc68f2e333a830c24211b
john-lay <john_lay@hotmail.co.uk>
2018-02-02 10:22:55 +0000

    Imported walking model and added animator and behaviour script so that the walkcycle begins and ends with a "w" keydown

---
+++ Unity/Tygra/Assets/Tygra/TygraBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TygraBehaviourScript : MonoBehaviour
+{
+    private Animator anim;
+    private bool isWalking;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("w pressed");
+            if (isWalking)
+            {
+                anim.SetTrigger("idle-trigger");
+            }
+            else
+            {
+                anim.SetTrigger("walk-trigger");
+            }
+
+            isWalking = !isWalking;
+        }
+    }
+}
