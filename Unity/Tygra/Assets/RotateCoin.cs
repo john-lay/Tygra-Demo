005dc172801a3e48d30d0cd2c6131519ec0f5aab
john-lay <john_lay@hotmail.co.uk>
2018-04-17 21:12:52 +0100

    Tidied up external assets folder. Created thundercoin asset with texture and rotate animation as prefab.

---
+++ Unity/Tygra/Assets/RotateCoin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateCoin : MonoBehaviour
+{
+    GameObject coin;
+
+    // Use this for initialization
+    void Start()
+    {
+        coin = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coin.transform.Rotate(new Vector3(0, 60, 0) * Time.deltaTime);
+    }
+}
