ccc39af50fd14ce06fa7e0367809e07fe163c0d6
john-lay <john_lay@hotmail.co.uk>
2018-05-22 14:18:56 +0100

    Add collectable coins
    Using kinematic rigid body and a simple script to allow coins to be collected.

---
+++ Unity/Tygra/Assets/CollectCoin.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    private GameObject coin;
+
+    // Use this for initialization
+    void Start()
+    {
+        coin = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);
+        Destroy(coin);
+    }
+}
