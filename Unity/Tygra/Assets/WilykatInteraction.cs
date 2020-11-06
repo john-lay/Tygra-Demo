da461678ab7d041def9f96ead6b82e5dccc20eed
john-lay <john_lay@hotmail.co.uk>
2020-11-06 15:59:38 +0000

    Add skeleton Wilykat interactions script

---
+++ Unity/Tygra/Assets/WilykatInteraction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WilykatInteraction : MonoBehaviour
+{
+
+    private GameObject wilykat;
+    private GameObject tygra;
+    private bool interact;
+    // Use this for initialization
+    void Start()
+    {
+        this.wilykat = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.interact && Input.GetButtonDown("Submit")) {
+            Debug.Log("talking to wilykit...");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.tygra = other.gameObject;
+        this.interact = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.interact = false;
+    }
+}
