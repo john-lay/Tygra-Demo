11b6029a3422e91b3e7e763bf7a1d8819c532c23
john-lay <john_lay@hotmail.co.uk>
2020-11-06 20:24:25 +0000

    Add Wilykat portrait to dialog box

--- Unity/Tygra/Assets/TextboxManager.cs
+++ Unity/Tygra/Assets/TextboxManager.cs
@@ -6,24 +6,37 @@ using UnityEngine.UI;
 public class TextboxManager : MonoBehaviour
 {
     private GameObject textboxContainer;
-    private Image image;
+    private Image textboxBg;
+    private Image wilykatPortrait;
     private bool visible;
     // Use this for initialization
     void Start()
     {
         this.textboxContainer = gameObject;
-        this.image = this.textboxContainer.GetComponentInChildren<Image>();
+        List<Image> images = new List<Image>(this.textboxContainer.GetComponentsInChildren<Image>());
+
+        if (images.Exists(img => img.name == "TextboxBg"))
+        {
+            this.textboxBg = images.Find(img => img.name == "TextboxBg");
+        }
+
+        if (images.Exists(img => img.name == "WilykatPortrait"))
+        {
+            this.wilykatPortrait = images.Find(img => img.name == "WilykatPortrait");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Submit")) this.ToggleVisibility();
+        if (Input.GetButtonDown("Submit")) this.ToggleVisibility();
     }
 
     void ToggleVisibility()
     {
-        this.image.enabled = this.visible;
+        this.textboxBg.enabled = this.visible;
+        this.wilykatPortrait.enabled = this.visible;
         this.visible = !this.visible;
     }
 }
