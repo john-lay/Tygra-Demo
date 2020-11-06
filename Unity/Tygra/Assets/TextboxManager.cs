2dce858941a79c2302245ae2acdaf35873b0acf8
john-lay <john_lay@hotmail.co.uk>
2020-11-06 18:40:00 +0000

    Add textbox and toggle visibility with Enter key

---
+++ Unity/Tygra/Assets/TextboxManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextboxManager : MonoBehaviour
+{
+    private GameObject textboxContainer;
+    private Image image;
+    private bool visible;
+    // Use this for initialization
+    void Start()
+    {
+        this.textboxContainer = gameObject;
+        this.image = this.textboxContainer.GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit")) this.ToggleVisibility();
+    }
+
+    void ToggleVisibility()
+    {
+        this.image.enabled = this.visible;
+        this.visible = !this.visible;
+    }
+}
